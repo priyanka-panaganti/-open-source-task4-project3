f83a25f885e16db073778a8ca9360e7f4c21deb9
panaganti priyanka <panagantipriyanka@gmail.com>
2022-02-07 09:30:34 +0530

    complete project

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,3 @@
+<template>
+  <footer>Made by <a href="">priyanka</a></footer>
+</template>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Match The Globe</h1>
+  
+</template>
+
+<style module>
+.description {
+  font-family: 'Titillium Web', sans-serif;
+}
+.description p {
+  margin: 0;
+  font-size: 1rem;
+}
+.description p:last-child {
+  margin-top: 10px;
+  margin-bottom: 30px;
+  font-size: 0.9rem;
+}
+.powered-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 15px;
+}
+.powered-link {
+  padding: 0 5px;
+}
+.powered-link:first-child,
+.powered-link:last-child {
+  padding-right: 0;
+}
+.powered-logo {
+  width: 18px;
+}
+.title {
+  padding-bottom: 30px;
+}
+</style>
